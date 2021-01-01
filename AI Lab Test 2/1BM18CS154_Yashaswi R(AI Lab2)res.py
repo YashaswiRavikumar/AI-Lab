4f6a6e2958fbd2d8e618b11c26cab5d8943d21b8
Yashaswi R <58076484+YashaswiRavikumar@users.noreply.github.com>
2021-01-01 15:02:20 +0530

    Update 1BM18CS154_Yashaswi R(AI Lab2)res.py

--- AI Lab Test 2/1BM18CS154_Yashaswi R(AI Lab2)res.py
+++ AI Lab Test 2/1BM18CS154_Yashaswi R(AI Lab2)res.py
@@ -1,35 +1,31 @@
 import re
-
-
 def negate(term):
-    return f"~{term}" if term[0] != "~" else term[1]
-
+    return f'{term}' if term[0] != '' else term[1]
 
 def reverse(clause):
-    if len(clause) > 2:
-        t = split_terms(clause)
-        return f"{t[1]}v{t[0]}"
-    return ""
-
+    t = split_terms(clause)
+    if len(t) > 2:
+        return f'{t[1]}v{t[0]}'
+    return ''
 
 def split_terms(rule):
-    exp = "(~*[PQRS])"
-    terms = re.findall(exp, rule)
+    # exp = '(*[V])'
+    # terms = re.findall(exp, rule)
+    terms = rule.split("v")
+    # print("te", terms)
     return terms
 
-
 def contradiction(query, clause):
-    contradictions = [f"{query}v{negate(query)}", f"{negate(query)}v{query}"]
+    contradictions = [ f'{query}v{negate(query)}', f'{negate(query)}v{query}']
     return clause in contradictions or reverse(clause) in contradictions
-
 
 def resolve(kb, query):
     temp = kb.copy()
     temp += [negate(query)]
     steps = dict()
     for rule in temp:
-        steps[rule] = "Given."
-    steps[negate(query)] = "Negated conclusion."
+        steps[rule] = 'Given.'
+    steps[negate(query)] = 'Negated conclusion.'
     i = 0
     while i < len(temp):
         n = len(temp)
@@ -45,54 +41,44 @@ def resolve(kb, query):
                     gen = t1 + t2
                     if len(gen) == 2:
                         if gen[0] != negate(gen[1]):
-                            clauses += [f"{gen[0]}v{gen[1]}"]
+                            clauses += [f'{gen[0]}v{gen[1]}']
                         else:
-                            if contradiction(query, f"{gen[0]}v{gen[1]}"):
-                                temp.append(f"{gen[0]}v{gen[1]}")
-                                steps[
-                                    ""
-                                ] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
+                            if contradiction(query,f'{gen[0]}v{gen[1]}'):
+                                temp.append(f'{gen[0]}v{gen[1]}')
+                                steps[''] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
                                 \nA contradiction is found when {negate(query)} is assumed as true. Hence, {query} is true."
                                 return steps
                     elif len(gen) == 1:
-                        clauses += [f"{gen[0]}"]
+                        clauses += [f'{gen[0]}']
                     else:
-                        if contradiction(query, f"{terms1[0]}v{terms2[0]}"):
-                            temp.append(f"{terms1[0]}v{terms2[0]}")
-                            steps[
-                                ""
-                            ] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
+                        if contradiction(query,f'{terms1[0]}v{terms2[0]}'):
+                            temp.append(f'{terms1[0]}v{terms2[0]}')
+                            steps[''] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
                             \nA contradiction is found when {negate(query)} is assumed as true. Hence, {query} is true."
                             return steps
             for clause in clauses:
-                if (
-                    clause not in temp
-                    and clause != reverse(clause)
-                    and reverse(clause) not in temp
-                ):
+                if clause not in temp and clause != reverse(clause) and reverse(clause) not in temp:
                     temp.append(clause)
-                    steps[clause] = f"Resolved from {temp[i]} and {temp[j]}."
+                    steps[clause] = f'Resolved from {temp[i]} and {temp[j]}.'
             j = (j + 1) % n
         i += 1
     return steps
 
-
 def resolution(kb, query):
-    kb = kb.split(" ")
+    kb = kb.split(' ')
     steps = resolve(kb, query)
-    print("\nStep\t|Clause\t|Derivation\t")
-    print("-" * 30)
+    print('\nStep\t|Clause\t|Derivation\t')
+    print('-' * 30)
     i = 1
     for step in steps:
-        print(f" {i}.\t| {step}\t| {steps[step]}\t")
+        print(f' {i}.\t| {step}\t| {steps[step]}\t')
         i += 1
-
-
 def main():
     print("Enter the kb:")
     kb = input()
     print("Enter the query:")
     query = input()
-    resolution(kb, query)
+    resolution(kb,query)
 
-main()
+if __name__ == "__main__":
+    main()
